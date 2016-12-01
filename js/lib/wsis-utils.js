730ace4c8ecef7625c7722a4c6ec9f5b0a3a6288
ahmd abos <ahmdabos@gmail.com>
2016-12-01 14:47:08 +0400

    add files

---
+++ js/lib/wsis-utils.js
@@ -0,0 +1,46 @@
+﻿define(['jquery', 'hogan', 'fi.dialog'], function ($, _, Dialog) {
+    'use strict';
+
+
+    var wsisUtils = function (el) {
+        this.$el = el;
+        this.$el.find(".view-list").on('click', $.proxy(this._openPopup, this));
+    };
+
+    wsisUtils.prototype = {
+        _openPopup: function (ev) {
+            if (ev) ev.preventDefault();
+            var index = $(ev.target).parent().find('.cat-title span').text();
+            this._initDialog('category' + index + 'Dialog', $("#cat" + index).find(".dialog-body").html());
+        },
+        _initDialog: function (dialogId, body) {
+            if (!this[dialogId]) {
+                this[dialogId] = new Dialog({
+                    showCaption: false,
+                    showShare: false,
+                    showHeader: true,
+                    showClose: true,
+                    closeClassName: 'icon icon-48 icon-close-g'
+                });
+                this[dialogId].$dlgBody.append(body);
+            }
+            this[dialogId].open();
+            $('html, body').animate({ scrollTop: 0 }, 500);
+        },
+        defaultOptions: {
+            templates: {
+            }
+        }
+    };
+
+    $.fn.WsisUtils = function () {
+        return this.each(function () {
+            var el = $(this);
+            new wsisUtils(el);
+        });
+    };
+
+    $(function () {
+        $('[data-toggle="wsis-utils"]').WsisUtils();
+    });
+});
